6be0703c08d684da1f034798682816caf952cf2f
lymphaofcolours <jorgegonzalezvet@gmail.com>
2022-06-10 21:39:14 +0100

    This actually works

--- seqClass.py
+++ seqClass.py
@@ -15,14 +15,14 @@ args = parser.parse_args()
 
 args.seq = args.seq.upper()                 # Note we just added this line
 if re.search('^[ACGTU]+$', args.seq):
-    if re.search('\W*(UT)\W*', args.seq):
+    if re.search('\W*(UT)\W*', args.seq):   # This regex searches specifically for pattern "UT" in case it's neither DNA nor RNA
         print ('The sequence is nothing')
-    elif re.search('\W*(TU)\W*', args.seq):
+    elif re.search('\W*(TU)\W*', args.seq):     # This regex searches specifically for the pattern "TU" in case it's neither DNA nor RNA
         print ('The sequence is nothing')
     elif re.search('T', args.seq):
         print ('The sequence is DNA')              # Specifies if it is DNA
     elif re.search('U', args.seq):
-        print ('The sequence is RNA')
+        print ('The sequence is RNA')              # Specifies if it is RNA
     else:
         print ('The sequence can be DNA or RNA')   # Specifies if it may be both
 else:
