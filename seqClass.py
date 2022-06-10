25fc2681801377773de95ac31a5a932839cf85a4
lymphaofcolours <jorgegonzalezvet@gmail.com>
2022-06-10 21:35:10 +0100

    This works

--- seqClass.py
+++ seqClass.py
@@ -14,13 +14,15 @@ if len(sys.argv) == 1:
 args = parser.parse_args()
 
 args.seq = args.seq.upper()                 # Note we just added this line
-if re.search('^[ACGTU]+$', args.seq):           # This searches for the strutcure of the query line
-    if re.search('T', args.seq):
+if re.search('^[ACGTU]+$', args.seq):
+    if re.search('\W*(UT)\W*', args.seq):
+        print ('The sequence is nothing')
+    elif re.search('\W*(TU)\W*', args.seq):
+        print ('The sequence is nothing')
+    elif re.search('T', args.seq):
         print ('The sequence is DNA')              # Specifies if it is DNA
     elif re.search('U', args.seq):
         print ('The sequence is RNA')
-    elif re.search('^[TU]+$', args.seq):
-	print ('The sequence is nothing')
     else:
         print ('The sequence can be DNA or RNA')   # Specifies if it may be both
 else:
